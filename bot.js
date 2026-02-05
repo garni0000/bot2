839c9d9e786c4a6f7c5a37c4eeb7c95ba5e329e2
garni0000 <eurahttps.gg@gmail.com>
2026-02-05 18:55:43 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const { Telegraf, Markup } = require('telegraf');
+const express = require('express');
+
+const bot = new Telegraf('2255');
+
+/* ================= KEEP ALIVE ================= */
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot actif');
+});
+
+app.listen(3000, () => {
+  console.log('Keep alive serveur lancé');
+});
+/* ============================================= */
+
+// /start
+bot.start(async (ctx) => {
+  await ctx.reply(
+`salut bienvenu au hack de rafa
+
+voici les 3 etatpe pour activer le scripte
+
+=Creer un compte pro Authentique avec le code promo FSRAFA uniquement sur Melbet ou 1xbet
+
+*en suite clic sur le bouton ouvrir le bot en bas tu met ton id de ton nouveau compte pour le synchroniser avec le bot  et le token tu met 2255`,
+    Markup.inlineKeyboard([
+      [Markup.button.url('📲 Ouvrir le bot', 't.me/gamesripte_bot/botscript)],
+      [Markup.button.url('🎥 Comment creer un compte Pro authentique', 'https://t.me/RAFAFSJUNIOR/5')]
+    ])
+  );
+
+  await ctx.reply('👇');
+});
+
+bot.launch();
+
+console.log('Bot en ligne...');
